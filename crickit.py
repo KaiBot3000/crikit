e5aecbc2fab9241c99559f7ead39d4bbebe816f4
Kai <riselikethemoon@gmail.com>
2015-07-24 17:06:30 -0700

    toggles view without reloading, uses ajax

--- crickit.py
+++ crickit.py
@@ -9,31 +9,35 @@ app.secret_key = 'crickits_chirping'
 
 @app.route('/crickit')
 def index_page():
+	temp = request.args.get('test_info')
 	return render_template('base_crickit.html')
 
-@app.route('/chirp', methods=['POST'])
+@app.route('/chirp') #use GET, because we're not changing anything serverside
 def chirp():
-	choice = request.form.get('user_choice')
-	chirp_time = '0'
+	test_info = request.args.get('test_info')
+	print test_info
+	# choice = request.args.get('user_choice')
+	# chirp_time = '0'
 
-	if choice == 'use_user_temperature':
-		temp = request.form.get('user_input')
-		chirp_time = str(chirp_calc(temp))
+	# if choice == 'use_user_temperature':
+	# 	temp = request.args.get('user_input')
+	# 	chirp_time = str(chirp_calc(temp))
 
-	else:
-		flash("I haven't made that yet :( ")
+	# else:
+	# 	flash("I haven't made that yet :( ")
 
-	return render_template('base_crickit.html', chirp_time=chirp_time)
+	# return render_template('base_crickit.html', chirp_time=chirp_time)
+
+	return render_template('base_crickit.html')
 
 
-
-def chirp_calc(temp):
-	# uses temp to determine time interval btwn chirps
-	temp = float(temp)
-	chirps_per_minute = 40 + 4 * (temp - 50)
-	#chirps_per_ms = chirps_per_minute * (1 / 60000)
-	chirp_time = 60000 / chirps_per_minute
-	return chirp_time
+# def chirp_calc(temp):
+# 	# uses temp to determine time interval btwn chirps
+# 	temp = float(temp)
+# 	chirps_per_minute = 40 + 4 * (temp - 50)
+# 	#chirps_per_ms = chirps_per_minute * (1 / 60000)
+# 	chirp_time = 60000 / chirps_per_minute
+# 	return chirp_time
 
 
 
