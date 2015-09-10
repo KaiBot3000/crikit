591287b554ede131c2dc172a32ef5e9e41c64ea2
Kai <riselikethemoon@gmail.com>
2015-09-10 15:50:54 -0700

    working on temp api call

--- crikit.py
+++ crikit.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, flash, request
 from flask_debugtoolbar import DebugToolbarExtension
+import os
+import requests
 
 app=Flask(__name__)
 
 app.secret_key = 'crickets_chirping'
+
+weather_key = os.environ['WEATHER_API_KEY']
+
+@app.route('/')
+def geolocate():
+
+	return render_template('geolocateplay.html')
 
 
 @app.route('/crikit')
@@ -21,7 +30,7 @@ def chirp():
 	temp = request.args.get('user_temp')
 
 	chirp_time = None
-	if choice == "use_user_temp":
+	if choice == 'use_user_temp':
 		chirp_time = chirp_calc(temp)
 
 	# else (user_choice would be get location)
@@ -33,10 +42,21 @@ def chirp():
 	return str(chirp_time) 
 
 
-# def get_temp(lat, long):
-# 	uses js geolocate coordinates and api to get temp
+def get_temp(lat, lon):
+	# uses js geolocate coordinates and api to get temp
+	api_url = 'api.openweathermap.org/data/2.5/weather' # ?lat=%s&lon=%s' % (lat, lon)
 
-# 	return temp
+	location = {}
+	location['lat'] = lat
+	location['lon'] = lon
+
+	weather_data = response.get(api_url, params=location)
+
+	temp_kelvin = weather_data['main']['main.temp']
+
+	temp_farenheit = (temp_kelvin - 273.15) * 1.8 + 32 
+
+	return temp_farenheit
 
 
 def chirp_calc(temp):
